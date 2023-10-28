0888a2be960efe1b1f691093e3fe8ca1a29da531
windlil <windlil@outlook.com>
2023-10-28 15:36:54 +0800

    feat: add new scripts

---
+++ utils/create/index.js
@@ -0,0 +1,20 @@
+import chalk from 'chalk'
+import { createComponent } from '../createComponent/create'
+import { createPage } from '../createTestVue/createPage'
+
+const componentsPath = './packages/components/src'
+
+function create() {
+  const commands = process.argv.splice(2)
+
+  if (commands.length === 0) {
+    console.log(chalk.red('缺少必要参数'))
+    return
+  }
+  for (const item of commands) {
+    createComponent(componentsPath, item)
+  }
+  createPage(commands)
+}
+
+create()
--- utils/createTestVue/index.js
+++ utils/createTestVue/index.js
@@ -1,56 +1,14 @@
-import fs from 'node:fs'
+import { createPage } from './createPage.js'
 import chalk from 'chalk'
-import '../../config/env.js'
 
-const pagesPath = './playground/vue-vite-playground/src/pages'
-
-function createTemplate() {
-  const css = process.env.CSS_PREPROCESSOR
-  return `<script setup lang="ts">
-
-</script>
-
-<template>
-  <div>
-    component
-  </div>
-</template>
-
-<style scoped lang="${css}">
-
-</style>
-`
-}
-
-function createPage() {
+function create() {
 
   const commands = process.argv.splice(2)
   if (commands.length === 0) {
     console.log(chalk.red('缺少必要参数'))
     return
   }
-  const template = createTemplate()  
-  for (let i = 0; i < commands.length; i++) {
-    const path = `${pagesPath}/${commands[i]}`
-    const target = `${path}/index.vue`
-  
-    fs.mkdir(path, {
-      recursive: true,
-    }, (err) => {
-      if (!err) {
-        fs.writeFile(target, template, (err) => {
-          if (!err) {
-            console.log(chalk.green('新测试组件生成完成，位置:', target))
-          } else {
-            console.log('新测试组件生成错误', err)
-          }
-        })
-      } else {
-        console.log('创建测试组件失败', err)
-        return
-      }
-    })
-  }
+  createPage(commands)
 }
 
-createPage()
+create()
