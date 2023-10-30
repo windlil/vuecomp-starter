f815d0c5f5281c656b2130ce9934b2a775723bfc
windlil <windlil@outlook.com>
2023-10-30 18:09:37 +0800

    feat: update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,19 +3,13 @@ import { sidebar } from './config/sidebar'
 
 export default defineConfig({
   title: `vuecomp-starter`,
-  description: 'Amazing UI 组件库',
+  description: 'vuecomp-starter',
   base: '/vuecomp-starter/',
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: 'logo.svg' }],
   ],
 
   themeConfig: {
-    algolia: {
-      appId: 'SHDNEYGA8Z',
-      apiKey: '91419401b0b0efd31b610e54e5b97249',
-      indexName: 'vue-amazing-ui'
-    },
-
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2023-present The Muse Catcher',
